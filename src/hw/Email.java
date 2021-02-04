548df9e95073d7997328e6495994c32bade2d4cb
Сергей <klimovichsergey122@gmail.com>
2021-02-05 00:08:46 +0300

    Initial

---
+++ src/hw/Email.java
@@ -0,0 +1,52 @@
+package hw;
+
+public class Email {
+    private   int id;
+    private String to;
+    private String from;
+    private String heading;
+    private String body;
+
+    public Email() {
+    }
+
+    public Email(int id, String to, String from, String heading, String body) {
+        this.id = id;
+        this.to = to;
+        this.from = from;
+        this.heading = heading;
+        this.body = body;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "id=" + id +
+                ", to='" + to + '\'' +
+                ", from='" + from + '\'' +
+                ", heading='" + heading + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
+
